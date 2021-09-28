e8ca6bd5724ebc2a3475835bf1679bad73be0e3c
PonchikCobaki <gsk124lan@gmail.com>
2021-09-29 00:36:34 +0300

    v 1.0

--- task 1/task 1.cpp
+++ task 1/task 1.cpp
@@ -1,29 +1,44 @@
 ﻿#include <iostream>
+#include <stdio.h>
+#include <stdarg.h>
 
 using namespace std;
 
-using someFnc = double (*)(someFnc SomeFunction, findMaxFnc FindMax, int n, ...);
-using findMaxFnc = double (*)(int n);
+using someFnc = double (*)(someFnc, findMaxFnc, int n, ...);
+using findMaxFnc = void (*)(double& max, int& num);
 
-double SomeFunction(someFnc SomeFunction, findMaxFnc FindMax, int n, ...) {
-    
-    double val = FindMax(n);
-    return val;
-}
+double SomeFunction(someFnc SomeFunction, findMaxFnc FindMax, int n, ...);
+void FindMax(double& max, int& num);
 
-double FindMax(int n) {
-    double accuracity = 1e-16;
-    double max = -1e16;
-    for (int* ptr = &n, int i = 0; n; --n, ++i) {
-        if (*(ptr + i) > max) {
-            max = *(ptr + i);
-        }
+
+
+int sum(int n, ...)
+{
+    int result = 0;
+    // получаем указатель на параметр n
+    for (int* ptr = &n; n > 0; n--)
+    {
+        result += *(++ptr);
     }
-
-    return max;
+    return result;
 }
 
 int main()
 {
-    cout << "max number: " << SomeFunction(SomeFunction, FindMax, 3, 1, 2, 3) << endl;
+    cout << "sum number: " << sum(3, 1, 2, 3) << endl;
+    //cout << "max number: " << SomeFunction(SomeFunction, FindMax, 3, 1, 2, 3) << endl;
+}
+
+double SomeFunction(someFnc SomeFunction, findMaxFnc FindMax, int n, ...) {
+    double max = -1e16;
+    for (int i = 0, int* ptr = &n; n > 0; n--, ++i) {
+        FindMax(max, *(ptr + i));
+    }
+    return max;
+}
+
+void FindMax(double& max, int& num) {
+    if (num > max) {
+        max = num;
+    }
 }
