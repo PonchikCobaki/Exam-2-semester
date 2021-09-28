5d9d00d2f38fafbf8f8e8eaf4f473ced91b42969
PonchikCobaki <gsk124lan@gmail.com>
2021-09-29 00:09:55 +0300

    debug

--- task 1/task 1.cpp
+++ task 1/task 1.cpp
@@ -2,14 +2,28 @@
 
 using namespace std;
 
+using someFnc = double (*)(someFnc SomeFunction, findMaxFnc FindMax, int n, ...);
+using findMaxFnc = double (*)(int n);
 
+double SomeFunction(someFnc SomeFunction, findMaxFnc FindMax, int n, ...) {
+    
+    double val = FindMax(n);
+    return val;
+}
 
+double FindMax(int n) {
+    double accuracity = 1e-16;
+    double max = -1e16;
+    for (int* ptr = &n, int i = 0; n; --n, ++i) {
+        if (*(ptr + i) > max) {
+            max = *(ptr + i);
+        }
+    }
+
+    return max;
+}
 
 int main()
 {
-
-
-
-
-    std::cout << "Hello World!\n";
+    cout << "max number: " << SomeFunction(SomeFunction, FindMax, 3, 1, 2, 3) << endl;
 }
