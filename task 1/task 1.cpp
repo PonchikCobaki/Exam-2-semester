02efbda0f7cf074448876032ebffb82ff6048bbc
PonchikCobaki <gsk124lan@gmail.com>
2021-09-29 13:24:15 +0300

    working version 1.0

--- task 1/task 1.cpp
+++ task 1/task 1.cpp
@@ -1,26 +1,28 @@
 ﻿#include <iostream>
-#include <stdio.h>
 #include <stdarg.h>
 
 using namespace std;
 
+// указатели на функции
 using findMaxFnc = double (*)(double num);
 using someFnc = double (*)(const findMaxFnc& FindMax, double n, ...);
 
+// прототипы функциий
 double FindMax(double num);
 double SomeFunction(const findMaxFnc& FindMax, double n, ...);;
 
 int main()
 {
     cout << "max number: " << SomeFunction(FindMax, 3, SomeFunction, 0.0042, 0.003) << endl;
+    cout << "max number: " << SomeFunction(FindMax, 5, SomeFunction, 0.0042, 0.003, 55544.0, 0, 0) << endl;
 }
 
 double SomeFunction(const findMaxFnc& FindMax, double n, ...) {
 
-    double (*SomeFunction)(const findMaxFnc & FindMax, double n, ...);
+    someFnc SomeFunctionPtr;
     va_list Arguments;
     va_start(Arguments, n);
-    SomeFunction = va_arg(Arguments, someFnc);  // указатель функции на саму себя
+    SomeFunctionPtr = va_arg(Arguments, someFnc);  // указатель функции на саму себя
     --n;
 
     double max(0);
@@ -29,11 +31,11 @@ double SomeFunction(const findMaxFnc& FindMax, double n, ...) {
         max = FindMax(va_arg(Arguments, double));
     
     va_end(Arguments);
+
     return max;
 }
 
 double FindMax(double num) {
-    cout << "number: " << num << endl;
     static double max = -1e16;
     if (num > max) {
         max = num;
